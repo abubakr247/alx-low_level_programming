aba6025dcd8f2d3e5826468747c9cf85fe29aa9a
abubakr247 <sarbakun@gmail.com>
2023-02-16 13:12:07 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.io>
+/**
+ * main - A program that print aline using the printf function
+ * Return: 0 (success)
+ */
+int main (void)
+{
+	prntf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
