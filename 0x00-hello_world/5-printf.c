ea2a9653338602b3648608edc285bdcf8a03552b
abubakr247 <sarbakun@gmail.com>
2023-02-16 13:30:43 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A program that print aline using the printf function
  * Return: 0(success)
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
