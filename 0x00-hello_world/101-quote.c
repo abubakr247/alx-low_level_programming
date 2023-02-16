104dbf45d1a75153da2bb67c1844d7928b52be3c
abubakr247 <sarbakun@gmail.com>
2023-02-16 14:34:27 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line stander error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("nd that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
