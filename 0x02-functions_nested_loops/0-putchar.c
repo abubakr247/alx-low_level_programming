9b9e43668b382dceb241f18f968c0f8a318d3dfc
abubakr247 <sarbakun@gmail.com>
2023-02-21 13:58:14 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * main a program prints the _putchar
  * Return: Always (Success)
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main a program prints the 1-alphabet.c
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+	for(c = 'a';c <= 'z'; c++)
+		putchar(c);
+	return (0);
+}
