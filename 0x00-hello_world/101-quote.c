e106a3f3b64879d3b7089c0ca8b89dd13115ad2b
abubakr247 <sarbakun@gmail.com>
2023-03-05 06:09:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line stander error
- * Return 1 (Success)
+ * main - Entr
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
