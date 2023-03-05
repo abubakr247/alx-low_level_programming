cebaa4efee8a9fcdcccebd073af9bd03aaf81c59
abubakr247 <sarbakun@gmail.com>
2023-03-05 06:01:28 -0800

    3-print_remaining_days.c, main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -25,10 +25,10 @@ int largest_number(int a, int b, int c)
         largest = b;
     }
      else
-    {
-        largest = c;
+     {
+	   largest = c;
     }
-
-  return (largest);
+     
+     return (largest);
 }
 
