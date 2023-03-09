7490538d3c6db019600b8a2df8eacb1ca27eedc7
abubakr247 <sarbakun@gmail.com>
2023-03-09 09:11:24 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _pow_recursion - Returns the value of x raised to the power of y
- * @x: the value to multiply
- * @y: the times to multiply the value
- *
- * Retrun: the value multiplied y times
+ * sqrt_mod - Recursive function to find a square root
+ * of a given number
+ * @n: Given number
+ * @i: Variable counter
+ * Return: Square root of a given number
  */
-int _pow_recursion(int x, int y)
+int sqrt_mod(int n, int i)
 {
-	if (y < 0)
+	if (i * i == n)
+		return (i);
+	i++;
+	if (i * i < n)
+		return (sqrt_mod(n, ++i));
+	if (i * i > n)
 		return (-1);
+	return (i);
+}
+/**
+ * _sqrt_recursion - Square root of a given number
+ * @n: Integer variable
+ * Return: Integer variable
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
 
-	if (y == 0)
-		return (1);
+	if (n == 1 || n == 0)
+		return (n);
 
-	return (x * _pow_recursion(x, y - 1));
+	return (sqrt_mod(n, i));
 }
