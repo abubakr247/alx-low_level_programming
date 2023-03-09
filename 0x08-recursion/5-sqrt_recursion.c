848995f3b2de3bd5dad83468d3a4efb853c3dd2b
abubakr247 <sarbakun@gmail.com>
2023-03-09 10:17:30 -0800

    help

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 
-int _sqrt(int n, int i);
-
 /**
- * _sqrt_recursion - Returns the natural square root of a number
- * @n: number to calculate the natural square root
- *
- * Return: the natural square root
+ * sqrt_mod - Recursive function to find a square root
+ * of a given number
+ * @n: Given number
+ * @i: Variable counter
+ * Return: Square root of a given number
+ */
+int sqrt_mod(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	i++;
+	if (i * i < n)
+		return (sqrt_mod(n, ++i));
+	if (i * i > n)
+		return (-1);
+	return (i);
+}
+/**
+ * _sqrt_recursion - Square root of a given number
+ * @n: Integer variable
+ * Return: Integer variable
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
+	int i = 1;
 
-/**
- * _sqrt - Calculates natural square root
- * @n: number to calculate the square root
- * @i: iterate number
- *
- * Return: the natural square root
- */
-int _sqrt(int n, int i)
-{
-	int sqrt = i * i;
-	if (sqrt > n)
-		return (-1);
-	if (sqrt == n)
-		return (i);
-	return (_sqrt(n, i + 1));
-}
+	if (n == 1 || n == 0)
+		return (n);
 
+	return (sqrt_mod(n, i));
+}
